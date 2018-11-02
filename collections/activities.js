c03ce2e6c8ae401c69150d4747f926181aa9fe1a
Michael Jung <michael@bluemeg.com>
2018-11-02 10:06:34 +0800

    first commit

---
+++ collections/activities.js
@@ -0,0 +1,22 @@
+import SimpleSchema from 'simpl-schema';
+
+// Placeholder database collection
+Activities = new Mongo.Collection('activities');
+
+const Schemas = {};
+
+Schemas.Activity = new SimpleSchema({
+  name: {
+    type: String,
+    label: 'name',
+  },
+});
+
+Activities.attachSchema(Schemas.Activity);
+
+// Deny client side database mutations
+Activities.deny({
+  insert() { return true; },
+  update() { return true; },
+  remove() { return true; },
+});
